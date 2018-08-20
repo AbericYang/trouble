a540612db58566b4a5bfc9ad14d30ff6c65cd44c
Aberic <100309595@qq.com>
2018-08-20 20:52:18 +0800

    Initial commit

---
+++ bother/src/main/java/cn/aberic/bother/BotherApplication.java
@@ -0,0 +1,12 @@
+package cn.aberic.bother;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BotherApplication.class, args);
+    }
+}
